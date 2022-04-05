3886022a7668cc48a166765fd996b0ade75c4240
Alex Ko <fyk211@nyu.edu>
2022-04-05 19:03:46 +0400

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const player3 = new Tone.Player("E.mp3").toDestination();
 const player4 = new Tone.Player("F.mp3").toDestination();
 const player5 = new Tone.Player("G.mp3").toDestination();
 
-players = [player1, player2, player3, player4, player5];
+let players = [player1, player2, player3, player4, player5];
 Tone.loaded().then(() => {
     for (let i=1; i<=5; i++){
         document.querySelectorAll(".cat")[i-1].addEventListener("click", function(event){
